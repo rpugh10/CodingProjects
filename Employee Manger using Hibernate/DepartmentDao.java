621471b2f012816d521708133de7ad10a33f0522
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-05-27 14:49:04 -0700

    Add files via upload

--- Employee Manger using Hibernate/DepartmentDao.java
+++ Employee Manger using Hibernate/DepartmentDao.java
@@ -1,5 +1,82 @@
 package jpa.example.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jpa.example.model.Department;
+import jpa.example.util.JPAUtil; 
+import java.util.*;
+
 public class DepartmentDao {
 
+    public void save(Department department)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+            try{
+                em.getTransaction().begin(); //Get transaction and start it
+                em.persist(department); // Send what is passed to the db
+                em.getTransaction().commit(); //Finalizes the transaction
+            }finally{
+                em.close();
+            }
+    }
+
+    public Department findById(Long id){
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            return em.find(Department.class, id); //find is a method the retrieves an entity by its class and primary key
+        }finally{
+            em.close();
+        }
+    }
+
+    public List<Department> findAll()
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            TypedQuery<Department> query = em.createQuery("SELECT d FROM Department d", Department.class);
+            return query.getResultList();
+        }finally{
+            em.close();
+        }
+    }
+
+    public void update(Department department)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.merge(department); //Updating the row in the db
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public void delete(Department department)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            Department toRemove = em.merge(department); //This makes sure the department is managed by the EntityManger 
+            em.remove(toRemove); //Then removes the department
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public Department findByName(String name)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            TypedQuery<Department> query = em.createQuery("SELECT d FROM Department d WHERE d.name = :name", Department.class);
+            query.setParameter("name", name);
+            List<Department> department = query.getResultList(); 
+            return department.isEmpty() ? null : department.get(0); //if department is empty return null else if department is not empty return index 0
+        }finally{
+            em.close();
+        }
+    }
+
+    
 }
--- Employee Manger using Hibernate/EmployeeDao.java
+++ Employee Manger using Hibernate/EmployeeDao.java
@@ -1,5 +1,81 @@
 package jpa.example.dao;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jpa.example.model.*;
+import jpa.example.util.JPAUtil;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 public class EmployeeDao {
 
+    public void save(Employee employee)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+            try{
+                em.getTransaction().begin();
+                em.persist(employee);
+                em.getTransaction().commit();
+            }finally{
+                em.close();
+            }
+    }
+
+    public Employee findById(Long id)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            return em.find(Employee.class, id);
+        }finally{
+            em.close();
+        }
+    }
+
+    public List<Employee> findAll()
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e", Employee.class);
+            return query.getResultList();
+        }finally{
+            em.close();
+        }
+    }
+
+    public void delete(Employee e)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            Employee toRemove = em.merge(e);
+            em.remove(toRemove);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public void update(Employee e)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            em.getTransaction().begin();;
+            em.merge(e);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public List<Employee> findSalaryGreaterThan(BigDecimal min)
+    {
+        EntityManager em = JPAUtil.getEntityManager();
+        try{ // Use :min that tells hibernate I will give you a value for min in the runtime
+            TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employees e WHERE e.salary > :min", Employee.class);
+            query.setParameter("min", min); //Sets the actual value for the named parameter in the query
+            return query.getResultList();
+        }finally{
+            em.close();
+        }
+    }
 }
